37dc1d36c6037050460b5cc259e699de3acc7a7c
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-06 18:54:48 +0100

    little improvements on r2

--- public/view/r2-animation.js
+++ public/view/r2-animation.js
@@ -1,22 +1,26 @@
-const elem = document.getElementById('r2d2-wrapper');
-elem.style.left = (window.screen.width / 2) - 100;
-elem.style.top = (window.screen.height / 5);
+const r2 = document.getElementById('r2d2-wrapper');
+const btn = document.getElementById('stop-btn');
+r2.style.left = (window.screen.width / 2) - 100;
+r2.style.top = (window.screen.height / 5);
 let endAnimation = false;
+let interval;
 
-setTimeout(() => move(), 2000);
-setTimeout(() => endAnimation = true, 20000);
+r2.addEventListener('click', move);
+btn.addEventListener('click', stop);
 
-function move(dir = 1) {
-    const start = parseInt(elem.style.left);
+function move(ev, dir = 1) {
+    btn.style.display = 'flex';
+    setTimeout(() => endAnimation = true, 20000);
+    const start = parseInt(r2.style.left);
     let pos = start + 1;
     let end = false;
     let speed = 4 * dir;
     let touchedEdges = 0;
-    const id = setInterval(() => {
-        if (end && Math.abs(parseInt(elem.style.left) - start) <= 5) {
-            clearInterval(id);
+    interval = setInterval(() => {
+        if (end && Math.abs(parseInt(r2.style.left) - start) <= 5) {
+            stop();
             const items = [-1, 1];
-            elem.style.left = (window.screen.width / 2) - 100;
+            r2.style.left = (window.screen.width / 2) - 100;
             !endAnimation && setTimeout(() => move(items[Math.floor(Math.random() * items.length)]), 1000);
         }
         if (pos <= 150 || pos >= (window.screen.width - 450)) {
@@ -25,6 +29,14 @@ function move(dir = 1) {
             if (touchedEdges == 2) end = true;
         }
         pos += speed;
-        elem.style.left = pos + 'px';
+        console.log('move', r2.style.left, dir);
+        r2.style.left = pos + 'px';
     }, 5);
+}
+
+function stop() {
+    clearInterval(interval);
+    btn.style.display = 'none';
+    r2.style.left = (window.screen.width / 2) - 100;
+    r2.style.top = (window.screen.height / 5);
 }
