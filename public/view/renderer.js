78f902e0303f5afec02aaa876dbeeb9c1fe29116
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-06 17:08:54 +0100

    move r2 move

--- public/view/renderer.js
+++ public/view/renderer.js
@@ -12,23 +12,62 @@ ws.onmessage = function (ev) {
     test.print = 'new';
     setTimeout(() => {
         Log(test);
-        ws.send(stringifyJSON(test));
+        // ws.send(stringifyJSON(test));
     }, 3000);
 };
 
-const canvas = document.getElementById('myCanvas');
+/* const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
-const maxWidth = 400;
-const lineHeight = 60;
-const x = (canvas.width - 340) / 2;
-const y = 70;
 const text = 'Beep bop boop beep beep beep!';
-
-context.font = '16pt Calibri';
-context.fillStyle = '#333';
-
 context.font = '45pt Calibri';
-context.fillStyle = '#82acdb';
+ */
+//CanvasTextWrapper(canvas, text, { font: context.font, textAlign: 'center', lineBreak: 'auto', allowNewLine: true });
+const elem = document.getElementById('r2d2-wrapper');
+elem.style.left = (window.screen.width / 2) - 100;
+elem.style.top = (window.screen.height / 5);
+setTimeout(() => myMove(), 2000);
 
-CanvasTextWrapper(canvas, text, { font: context.font, textAlign: 'center', lineBreak: 'auto', allowNewLine: true });
-//wrapText(context, text, x, y, maxWidth, lineHeight);
+function myMove() {
+    const start = parseInt(elem.style.left);
+    let pos = start + 1;
+    let end = false;
+    let speed = 4;
+    const id = setInterval(() => {
+        if (end && Math.abs(parseInt(elem.style.left) - start) <= 5) {
+            clearInterval(id);
+        }
+        if (pos <= 150 || pos >= (window.screen.width - 450)) {
+            speed = -speed;
+            if (pos <= 150) end = true;
+        }
+        pos += speed;
+        elem.style.left = pos + 'px';
+    }, 5);
+}
+
+/* let x = 0;
+const y = 15;
+let speed = 0.3;
+speed = 0.3;
+function animate() {
+    requestAnimationFrame(animate);
+    x += speed;
+    if (x <= 0 || x >= 475) {
+        speed = -speed;
+        // y = y + 10; so könnte das Objekt am Ende einer Zeile eine Zeile nach unten gehen
+    }
+    draw();
+}
+
+function draw() {
+    Log('draw');
+    context.clearRect(0, 0, 500, 300);
+    context.fillStyle = '#dfac20';
+    context.fillRect(x, y, 20, 20);
+    context.lineWidth = 3;
+    context.strokeStyle = '#3983ab';
+    context.strokeRect(x, y, 20, 20);
+}
+document.addEventListener('DOMContentLoaded', function () {
+    animate();
+}); */
