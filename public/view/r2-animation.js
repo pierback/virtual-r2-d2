59aec8db55b617fe2a6d8190409ca0add5017985
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-07 00:57:47 +0100

    Implemented move-ball feature

--- public/view/r2-animation.js
+++ public/view/r2-animation.js
@@ -2,63 +2,105 @@
 const { log } = require('/scripts/helper.js');
 const r2 = document.getElementById('r2d2-wrapper');
 const btn = document.getElementById('stop-btn');
+const ball = document.getElementById('ball');
+const ballBtn = document.getElementById('ball-btn');
 const offsets = r2.getBoundingClientRect();
 const r2PosX = () => parseInt(r2.style.left);
+const r2PosY = () => parseInt(r2.style.top);
 const startPosX = parseInt(offsets.left);
-let endAnimation = false;
+const startPosY = parseInt(offsets.top);
+const endAnimation = false;
 let interval, speed;
-setPosition();
+let blockClick = false;
+let ballDisp = false;
+setR2Position();
+setBallPos();
 
-r2.addEventListener('click', move);
+r2.addEventListener('click', () => {
+    !blockClick && move();
+});
+ballBtn.addEventListener('click', () => {
+    if (!blockClick) {
+        ballDisp = true;
+        move();
+    }
+});
 btn.addEventListener('click', stop);
 
-function move(ev, dir = 1) {
-    btn.style.display = 'flex';
-    let pos = startPosX + 1;
-    let end = false;
-    let touchedEdges = 0;
-    speed = 4 * dir;
-    setTimeout(() => endAnimation = true, 20000);
-    interval = setInterval(() => {
-        if (end && Math.abs(r2PosX() - startPosX) <= 5) {
-            stop();
-            !endAnimation && setTimeout(() => move(randSign), 1000);
-        }
-        if (pos <= 150 || pos >= (window.screen.width - 450)) {
-            speed = -speed;
-            touchedEdges++;
-            if (touchedEdges == 2) end = true;
-        }
-        log('click', pos, speed);
-        pos += speed;
-        r2.style.left = pos + 'px';
-    }, 5);
+
+
+function setBallPos() {
+    ball.style.left = startPosX + 18 + 200 + 'px';
+    ball.style.top = startPosY + 175 + 'px';
 }
 
-function setPosition() {
-    r2.style.left = startPosX;//(window.screen.width / 2) - 100;
-    //r2.style.top = (window.screen.height / 5);
+function moveBall(dir) {
+    ball.style.display = 'inline';
+    ballBtn.disabled = true;
+    let direction;
+    dir < 0 ? direction = 200 : direction = -75;
+    ball.style.left = r2PosX() + 18 + (direction) + 'px';
+    ball.style.top = r2PosY() + 175 + 'px';
+}
+
+function switchSide() {
+    ball.style.left = r2PosX() + 75 + 'px';
+    ball.style.top = r2PosY() - 175 + 'px';
+}
+
+function move(ev, dir = 1) {
+    if (!blockClick) {
+        blockClick = true;
+        let pos = startPosX + 1;
+        let end = false;
+        let touchedEdges = 0;
+        speed = 4 * dir;
+        interval = setInterval(() => {
+            if (end && Math.abs(r2PosX() - startPosX) <= 15) {
+                blockClick = false;
+                stop();
+            }
+            if (pos <= 150 || pos >= (window.screen.width - 450)) {
+                speed = -speed;
+                touchedEdges++;
+                if (touchedEdges == 4) end = true;
+            }
+            pos += speed;
+            r2.style.left = pos + 'px';
+            ballDisp && moveBall(-speed);
+        }, 5);
+    }
+}
+
+function setR2Position() {
+    r2.style.left = startPosX;
 }
 
 function stop() {
     clearInterval(interval);
-    btn.disabled = true;
     let curPos = r2PosX();
     r2PosX() > startPosX ? speed = Math.abs(speed) * -1 : speed = Math.abs(speed);
 
     const stopInter = setInterval(() => {
-        if (Math.abs(r2PosX() - startPosX) <= 15) {
+        if (Math.abs(r2PosX() - startPosX) <= 5) {
             speed /= Math.abs(speed);
         }
         if (r2PosX() === startPosX) {
             clearInterval(stopInter);
-            btn.style.display = 'none';
-            btn.disabled = false;
+            hideBall();
         }
         curPos += speed;
         r2.style.left = curPos + 'px';
+        ballDisp && moveBall(speed);
     });
 }
+
+function hideBall() {
+    ballDisp = false;
+    ball.style.display = 'none';
+    ballBtn.disabled = false;
+}
+
 
 function randSign() {
     const items = [-1, 1];
--- public/view/renderer.js
+++ public/view/renderer.js
@@ -16,38 +16,3 @@ ws.onmessage = function (ev) {
         // ws.send(stringifyJSON(test));
     }, 3000);
 };
-
-/* const canvas = document.getElementById('myCanvas');
-const context = canvas.getContext('2d');
-const text = 'Beep bop boop beep beep beep!';
-context.font = '45pt Calibri';
- */
-//CanvasTextWrapper(canvas, text, { font: context.font, textAlign: 'center', lineBreak: 'auto', allowNewLine: true });
-
-
-/* let x = 0;
-const y = 15;
-let speed = 0.3;
-speed = 0.3;
-function animate() {
-    requestAnimationFrame(animate);
-    x += speed;
-    if (x <= 0 || x >= 475) {
-        speed = -speed;
-        // y = y + 10; so könnte das Objekt am Ende einer Zeile eine Zeile nach unten gehen
-    }
-    draw();
-}
-
-function draw() {
-    Log('draw');
-    context.clearRect(0, 0, 500, 300);
-    context.fillStyle = '#dfac20';
-    context.fillRect(x, y, 20, 20);
-    context.lineWidth = 3;
-    context.strokeStyle = '#3983ab';
-    context.strokeRect(x, y, 20, 20);
-}
-document.addEventListener('DOMContentLoaded', function () {
-    animate();
-}); */
