2963673a52ded9cef70b3143cce37b0c610de65a
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-06 19:05:22 +0100

    refactored r2 code

--- public/view/r2-animation.js
+++ public/view/r2-animation.js
@@ -1,27 +1,26 @@
 const r2 = document.getElementById('r2d2-wrapper');
 const btn = document.getElementById('stop-btn');
-r2.style.left = (window.screen.width / 2) - 100;
-r2.style.top = (window.screen.height / 5);
 let endAnimation = false;
 let interval;
 
 r2.addEventListener('click', move);
 btn.addEventListener('click', stop);
 
+setPosition();
+
 function move(ev, dir = 1) {
     btn.style.display = 'flex';
-    setTimeout(() => endAnimation = true, 20000);
     const start = parseInt(r2.style.left);
     let pos = start + 1;
     let end = false;
     let speed = 4 * dir;
     let touchedEdges = 0;
+    setTimeout(() => endAnimation = true, 20000);
     interval = setInterval(() => {
         if (end && Math.abs(parseInt(r2.style.left) - start) <= 5) {
             stop();
-            const items = [-1, 1];
-            r2.style.left = (window.screen.width / 2) - 100;
-            !endAnimation && setTimeout(() => move(items[Math.floor(Math.random() * items.length)]), 1000);
+            setPosition();
+            !endAnimation && setTimeout(() => move(randSign), 1000);
         }
         if (pos <= 150 || pos >= (window.screen.width - 450)) {
             speed = -speed;
@@ -29,14 +28,22 @@ function move(ev, dir = 1) {
             if (touchedEdges == 2) end = true;
         }
         pos += speed;
-        console.log('move', r2.style.left, dir);
         r2.style.left = pos + 'px';
     }, 5);
+}
+
+function setPosition() {
+    r2.style.left = (window.screen.width / 2) - 100;
+    r2.style.top = (window.screen.height / 5);
 }
 
 function stop() {
     clearInterval(interval);
     btn.style.display = 'none';
-    r2.style.left = (window.screen.width / 2) - 100;
-    r2.style.top = (window.screen.height / 5);
+    setPosition();
+}
+
+function randSign() {
+    const items = [-1, 1];
+    return items[Math.floor(Math.random() * items.length)];
 }
