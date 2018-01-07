fa849ecfe9ceccf048729712b4c3a71252e0b989
Klaus Em <Klaus.Em@posteo.org>
2018-01-07 19:06:48 +0100

    Getter & setters of robot - will soon be implemented

--- public/view/robot.js
+++ public/view/robot.js
@@ -6,17 +6,67 @@ class Robot {
     _initializeVariables(env){
         const canvas = env.canvas;
         const canvasOffset = canvas.getBoundingClientRect();
-        this.leftBorder = canvasOffset.left;
-        this.rightBorder = canvasOffset.right;
-        this.topBorder = canvasOffset.bottom + 250; //temporary estimation (should be horizontal line in image)
+        this._leftBorder = canvasOffset.left;
+        this._rightBorder = canvasOffset.right;
+        this._topBorder = canvasOffset.bottom + 250; //temporary estimation (should be horizontal line in image)
+
+        console.log(canvasOffset);
 
         const robotOffset = env.robot.getBoundingClientRect();
         console.log(robotOffset);
-        this.x = robotOffset.left;
-        this.y = robotOffset.top;
-        this.height = robotOffset.height;
-        this.width = robotOffset.Offset.width
+        this._x = robotOffset.left;
+        this._y = robotOffset.top;
+        this._height = robotOffset.height;
+        this._width = robotOffset.Offset.width;
     }
+
+    //returns pixels of right canvas border - width of robot
+    getMaxX(){
+        return this._rightBorder - this._width;
+    }
+
+    //returns pixels of left canvas border
+    getMinX(){
+        return this._leftBorder;
+    }
+
+    //returns pixels of bottom canvas border + height of robot
+    getMaxY(){
+        
+    }
+
+    //returns pixels of horizontal line in image + half of the height of the robot
+    getMinY(){
+
+    }
+
+    getX(){
+
+    }
+
+    getY(){
+
+    }
+
+    //get width of robot
+    getWidth(){
+
+    }
+
+    //get height of robot
+    getHeight(){
+
+    }
+
+    setX(x){
+
+    }
+
+    setY(y){
+
+    }
+
+
 }
 
 exports.Robot = Robot;
