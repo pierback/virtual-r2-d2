938ce5afd5f3d83d5d592e6e0ab079a9859d3e94
Klaus Em <Klaus.Em@posteo.org>
2018-01-07 19:28:38 +0100

    merging

--- public/view/robot.js
+++ public/view/robot.js
@@ -8,16 +8,21 @@ class Robot {
         const canvasOffset = canvas.getBoundingClientRect();
         this._leftBorder = canvasOffset.left;
         this._rightBorder = canvasOffset.right;
+        this._bottomBorder = canvasOffset.bottom;
         this._topBorder = canvasOffset.bottom + 250; //temporary estimation (should be horizontal line in image)
-
-        console.log(canvasOffset);
 
         const robotOffset = env.robot.getBoundingClientRect();
         console.log(robotOffset);
         this._x = robotOffset.left;
         this._y = robotOffset.top;
+
+        env.robot.style.left = 250;
+        console.log(this._x);
+        console.log(env.robot.getBoundingClientRect().left);
+        console.log(env.robot.style.left);
+
         this._height = robotOffset.height;
-        this._width = robotOffset.Offset.width;
+        this._width = robotOffset.width;
     }
 
     //returns pixels of right canvas border - width of robot
@@ -32,16 +37,17 @@ class Robot {
 
     //returns pixels of bottom canvas border + height of robot
     getMaxY(){
-        
+        return this._bottomBorder + this._height;
     }
 
     //returns pixels of horizontal line in image + half of the height of the robot
     getMinY(){
-
+        const horizontalLine = (this._bottomBorder - this._topBorder) * 0.61;
+        return this._topBorder + horizontalLine + this.height/2;
     }
 
     getX(){
-
+        //return this.
     }
 
     getY(){
@@ -65,8 +71,6 @@ class Robot {
     setY(y){
 
     }
-
-
 }
 
 exports.Robot = Robot;
