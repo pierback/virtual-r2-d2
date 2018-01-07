588d42b0e7976f2451ff1a796481c86279ce8965
Klaus Em <Klaus.Em@posteo.org>
2018-01-07 12:46:39 +0100

    Scaled down robot further, added canvas offset

--- public/view/r2-animation.js
+++ public/view/r2-animation.js
@@ -1,10 +1,15 @@
 //@ts-check
-const { log } = require('/scripts/helper.js');
+//const { log } = require('/scripts/helper.js');
+
 const r2 = document.getElementById('r2d2-wrapper');
 const btn = document.getElementById('stop-btn');
 const ball = document.getElementById('ball');
 const ballBtn = document.getElementById('ball-btn');
+const canvas = document.getElementById('canvas');
+const canvasOffset = canvas.getBoundingClientRect();
+console.log(canvasOffset);
 const offsets = r2.getBoundingClientRect();
+console.log(offsets)
 const r2PosX = () => parseInt(r2.style.left);
 const r2PosY = () => parseInt(r2.style.top);
 const startPosX = parseInt(offsets.left);
@@ -30,11 +35,13 @@ btn.addEventListener('click', stop);
 
 
 function setBallPos() {
+    console.log("setBallPos");
     ball.style.left = startPosX + 18 + 200 + 'px';
-    ball.style.top = startPosY + 175 + 'px';
+    ball.style.top = startPosY + 100 + 'px';
 }
 
 function moveBall(dir) {
+    console.log("moveBall");
     ball.style.display = 'inline';
     ballBtn.disabled = true;
     let direction;
@@ -44,11 +51,13 @@ function moveBall(dir) {
 }
 
 function switchSide() {
+    console.log("switchSide");
     ball.style.left = r2PosX() + 75 + 'px';
     ball.style.top = r2PosY() - 175 + 'px';
 }
 
 function move(ev, dir = 1) {
+    console.log("move");
     if (!blockClick) {
         blockClick = true;
         let pos = startPosX + 1;
@@ -60,7 +69,7 @@ function move(ev, dir = 1) {
                 blockClick = false;
                 stop();
             }
-            if (pos <= 150 || pos >= (window.screen.width - 450)) {
+            if (pos <= canvasOffset.left || pos >= (canvasOffset.right - 450)) {
                 speed = -speed;
                 touchedEdges++;
                 if (touchedEdges == 4) end = true;
@@ -73,10 +82,12 @@ function move(ev, dir = 1) {
 }
 
 function setR2Position() {
+    console.log("setR2Position");
     r2.style.left = startPosX;
 }
 
 function stop() {
+    console.log("stop");
     clearInterval(interval);
     let curPos = r2PosX();
     r2PosX() > startPosX ? speed = Math.abs(speed) * -1 : speed = Math.abs(speed);
@@ -96,6 +107,7 @@ function stop() {
 }
 
 function hideBall() {
+    console.log("hideBall");
     ballDisp = false;
     ball.style.display = 'none';
     ballBtn.disabled = false;
@@ -103,6 +115,7 @@ function hideBall() {
 
 
 function randSign() {
+    console.log("randSign");
     const items = [-1, 1];
     return items[Math.floor(Math.random() * items.length)];
 }
