b2215fc67b5aee722afbf64eb1a9c2d44529774d
Klaus Em <Klaus.Em@posteo.org>
2018-01-08 01:40:22 +0100

    Fixed x and y setter and getter of robot

--- public/view/robot.js
+++ public/view/robot.js
@@ -10,89 +10,118 @@ class Robot {
         const canvasWidth = canvasOffset.width;
         const canvasHeight = canvasOffset.height;
 
-        const robotLegWidth = 52;
-        const robotStripeHeight = 45;
-        const robotWidth = 225;
-        //this.fullWidth = robotWidth + robotLegWidth * 2;
-
+        const robotOriginalLegWidth = 52; //original value
+        const robotOriginalWidth = 225; // original value
         const robotOffset = env.robot.getBoundingClientRect();
         console.log(robotOffset);
-        const scale = robotOffset.width / robotWidth;
-        const robotHeight = robotOffset.height / scale;
+        const xScale = robotOffset.width / robotOriginalWidth;
+        this._xCorrection = Math.floor((robotOriginalWidth - robotOffset.width) / 2 - robotOriginalLegWidth * xScale);
+        this._width = Math.floor(robotOffset.width + robotOriginalLegWidth * 2 * xScale);
+        this._minX = 0;
+        this._maxX = Math.floor(canvasWidth - this._width);
+        this._startX = Math.floor((this._maxX /2) - this._width);
+        this.X = this._startX;
 
-        this._xCorrection = (robotWidth - robotOffset.width) / 2 - robotLegWidth * scale;
-
-        this._height = robotOffset.height + robotStripeHeight * scale;
-        this._width = robotOffset.width + robotLegWidth * 2 * scale;
-        this._x = robotOffset.left - robotLegWidth * scale;
-        this._y = robotOffset.top;
-
-        const horizontalLine = (canvasHeight) * 0.61;
-        //this._minY = horizontalLine - this._height/2;
-        //this._maxY = canvasHeight - this._height;
-        this._minY = 80;
-        this._maxY = 400;
-
-        //console.log('min y ' + this._minY);
-        //console.log('max y ' + this._maxY);
-
-        this._maxX = canvasWidth - this._width;
-    }
-
-    //returns pixels of right canvas border - width of robot
-    get MaxX() {
-        return this._maxX;
+        const robotHeight = robotOffset.height;
+        const robotNewLegHeight = 19; //estimation for scale 0.3
+        const horizontalLine = (canvasHeight) * 0.61; //specific for desert image
+        this._yCorrection = 158; //estimation needs to be changed if scale is changed
+        this._height = Math.floor(robotHeight + robotNewLegHeight);
+        this._minY = Math.floor(horizontalLine - robotHeight /2);
+        this._maxY = Math.floor(canvasHeight - robotHeight - robotNewLegHeight);
+        this._startY = this._minY + Math.floor((this._maxY - this._minY)/2);
+        this.Y = this._startY;
     }
 
     //returns pixels of left canvas border
+    //OUTPUT: Integer
     get MinX() {
         return 0;
     }
 
-    //returns pixels of bottom canvas border + height of robot
-    get MaxY() {
-        return this._maxY;
+    //returns pixels of right canvas border - width of robot
+    //Output: Integer
+    get MaxX() {
+        return this._maxX;
     }
 
-    //returns pixels of horizontal line in image + half of the height of the robot
-    get MinY() {
-        return this._minY;
-    }
-
+    //returns current x position
+    //Output: Integer (between 0 and maxX)
     get X() {
-        return this._x;
-    }
-
-    get Y() {
-        return this._y;
+        return this._x + this._xCorrection;
     }
 
     //get width of robot
+    //Output: Integer
     get Width() {
         return this._width;
     }
 
+    //get start position (left from center)
+    //Output: Integer
+    get StartX() {
+        return this._startX;
+    }
+
+    //Input: Integer (0 robot is on the left border, MaxX robot is on the right border)
+    set X(x) {
+        console.log('input: ' + x);
+        if(isNaN(x)){
+            console.log('Error: Your value is not a number.');
+        } else {
+            x = parseInt(x);
+            if(x < this._minX) x = this._minX + 1;
+            if(x > this._maxX) x = this._maxX - 1;
+            this._x = x - this._xCorrection;
+            this.robot.style.left = this._x;
+        }
+    }
+
+    //----------- Y VALUES -------------------------------
+
+    //returns pixels of horizontal line in image + half of the height of the robot
+    //OUTPUT: Integer
+    get MinY() {
+        return this._minY;
+    }
+
+    //returns pixels of bottom canvas border + height of robot
+    //OUTPUT: Integer
+    get MaxY() {
+        return this._maxY;
+    }
+
+    //returns default start pos
+    //OUTPUT: Integer
+    get StartY() {
+        return this._startY;
+    }
+
     //get height of robot
+    //OUTPUT: Integer
     get Height() {
         return this._height;
     }
 
-    set X(x) {
-        console.log(x);
-        this._x = x - this._xCorrection;
-        console.log(this._x);
-        this.robot.style.left = this._x;
+    //get current y position
+    //OUTPUT: Integer
+    get Y(){
+        return this._y + this._yCorrection;
     }
 
+    //set y position
+    //INPUT: Integer
     set Y(y) {
-        if (y < this._minY) {
-            y = this._minY;
-        } else if (y > this._maxY) {
-            y = this._maxY;
+        if(isNaN(y)){
+            console.log('Error: Your value is not a number.');
+        } else {
+            y = parseInt(y);
+            if(y < this._minY) y = this._minY + 1;
+            if(y > this._maxY) y = this._maxY - 1;
+            this._y = y - this._yCorrection;
+            this.robot.style.top = this._y;
         }
-
-        this._y = y;
-        this.robot.style.top = y;
     }
 }
+
 exports.Robot = Robot;
