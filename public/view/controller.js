9a320cee79485d7cf26575753a65ddd1807c24b8
Klaus Em <Klaus.Em@posteo.org>
2018-01-11 17:56:00 +0100

    Restructured notes, added window resize listener

--- public/view/controller.js
+++ public/view/controller.js
@@ -19,6 +19,10 @@ class Controller {
         this._initializeSocket();
     }
 
+    updateCanvas(){
+        this.robot.updateSize();
+    }
+
     _initializeSocket() {
         const that = this;
         this.ws = new WebSocket('ws://localhost:4000');
@@ -148,4 +152,9 @@ class Controller {
             });
     }
 
-} new Controller();
+}
+c = new Controller();
+
+window.onresize = function(event) {
+    c.updateCanvas();
+};
