c71c32aa78dc958e4e34d36125acf9a6e838abc0
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-12 21:39:44 +0100

    changed port

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
     name: 'API',
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3200,
     base_url: process.env.BASE_URL || 'http://localhost'
 };
