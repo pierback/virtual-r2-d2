97003aeadd6c7c27404c68baefa5f8b215982834
Fabian <fabian.pieringer@student.uni-augsburg.de>
2018-01-12 22:18:40 +0100

    refactored server/websockets

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 //@ts-check
+const { parseJSON, stringifyJSON, getAllMethods, log } = require('./public/scripts/helper.js');
+const { Act } = require('./public/view/actions/act.js');
+const config = require('./config');
 const express = require('express');
 const app = express();
-const config = require('./config');
-const path = require('path');
-const { parseJSON, stringifyJSON, getAllMethods } = require('./public/scripts/helper.js');
-const { Act } = require('./public/view/actions/act.js');
-const act = new Act();
-const items = getAllMethods(act);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
+const path = require('path');
+
+const act = new Act();
+const items = getAllMethods(act);
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/view/environment/index.html');
 });
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     const item = items[Math.floor(Math.random() * items.length)];
-    console.log(item);
+    log(item);
     ws.send(item.toString());
   });
 });
 
 server.listen(config.port, function listening() {
-  console.log('Listening on %d', server.address().port);
+  log('Listening on %d', server.address().port);
 });
